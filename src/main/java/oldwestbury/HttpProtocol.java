0554704425fcd91ceda7632bcb1be6a3b194db10
mm <user@MohamedlefasAir.home>
2020-11-24 04:42:31 -0500

    repeating

--- src/main/java/oldwestbury/HttpProtocol.java
+++ src/main/java/oldwestbury/HttpProtocol.java
@@ -9,55 +9,56 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class HttpProtocol {
+  static String path = "/Users/user/eclipse-workspace/oldwestbury/http/";
 
-	static String path = "/Users/user/eclipse-workspace/oldwestbury/http/";
+  static String split(String s) throws InvalidRequest {
+    String[] parts = s.split(" ");
+    if (parts.length != 3)
+      throw new InvalidRequest();
+    if (!parts[2].toLowerCase().startsWith("http"))
+      throw new InvalidRequest();
+    if (!parts[0].equalsIgnoreCase("get"))
+      throw new InvalidRequest();
 
-	static String split(String s) throws InvalidRequest {
-		String[] parts = s.split(" ");
-		if (parts.length != 3)
-			throw new InvalidRequest();
-		if (!parts[2].toLowerCase().startsWith("http"))
-			throw new InvalidRequest();
-		if (!parts[0].equalsIgnoreCase("get"))
-			throw new InvalidRequest();
+    return parts[1];
+  }
 
-		return parts[1];
-	}
+  static void process(BufferedReader in, OutputStream out) throws IOException {
+    // reading the request
+    String get_request = "";
 
-	static void process(BufferedReader in, OutputStream out) throws IOException {
-		// reading the request
-		String get_request = "";
+    for (int i = 0;; i++) {
+      String l = in.readLine();
+      if (i == 0)
+        get_request = l;
+      if (l.trim().length() == 0)
+        break;
+    }
 
-		for (int i = 0;; i++) {
-			String l = in.readLine();
-			if (i == 0)
-				get_request = l;
-			if (l.trim().length() == 0)
-				break;
-		}
+    System.out.print(get_request);
 
-		System.out.print(get_request);
+    try {
+      String filepath = split(get_request);
+      System.out.println("--" + filepath + " ");
+      File f = new File(path + filepath);
+      if (f.exists() && f.isFile()) {
+        Path p = Paths.get(path, filepath);
+        out.write(("HTTP/1.1 "
+            + "200 OK"
+            + "\r\n")
+                      .getBytes());
+        out.write("\r\n".getBytes()); // Important to end the header
+        out.write(Files.readAllBytes(p));
 
-		try {
-			String filepath = split(get_request);
-			System.out.println("--" + filepath + " ");
-			File f = new File(path + filepath);
-			if (f.exists() && f.isFile()) {
-				Path p = Paths.get(path, filepath);
-				out.write(("HTTP/1.1 " + "200 OK" + "\r\n").getBytes());
-				out.write("\r\n".getBytes());
-				out.write(Files.readAllBytes(p));
-				out.write("\r\n\r\n".getBytes());
-				out.flush();
+        out.flush();
 
-			} else {
-				System.out.println("File does not exists");
-				String response = "HTTP/1.1 404 NOT FOUND\r\n";
-				out.write(response.getBytes());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+      } else {
+        System.out.println("File does not exists");
+        String response = "HTTP/1.1 404 NOT FOUND\r\n";
+        out.write(response.getBytes());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/main/java/oldwestbury/WebServerThread.java
+++ src/main/java/oldwestbury/WebServerThread.java
@@ -1,29 +1,31 @@
 package oldwestbury;
 
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
 public class WebServerThread extends Thread {
-	private Socket socket = null;
+  private Socket socket = null;
 
-	public WebServerThread(Socket socket) {
-		super("WebServerThread");
-		this.socket = socket;
-	}
+  public WebServerThread(Socket socket) {
+    super("WebServerThread");
+    this.socket = socket;
+  }
 
-	public void run() {
+  public void run() {
+	
+    try (OutputStream out = socket.getOutputStream();
+         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+   for(;;) {  
+	   socket.setSoTimeout(1000);
+	   System.out.println("  waiting");
+	   HttpProtocol.process(in, out);
+   	}
+   
+   //  socket.close();
+    }
 
-		try (OutputStream out = socket.getOutputStream();
-				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
-
-			HttpProtocol.process(in, out);
-
-			socket.close();
-		}
-
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
